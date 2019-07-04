7a44fad5687a6456182bb1cd091f241266f6a849
TheProfms <49717507+TheProfms@users.noreply.github.com>
2019-07-05 00:41:05 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,28 +1,39 @@
 import os
 import time
+import shutil
 
-file = open("synctime.txt", "r") 
-try:
-    lasttime = float(file.readline())
-except:
-    lasttime = 0.0
-file.close()
+def Check_Files(path,currfolder):
+    currfolder = currfolder + os.path.basename(os.path.normpath(path)) + "\\"
+    paths = os.listdir(path)
+    for file in paths:
+        if os.path.isfile(os.path.join(path, file)):
+            if os.path.getmtime(os.path.join(path, file)) > lasttime:
+                if not os.path.isdir(os.path.join(server_path,currfolder)):
+                    os.makedirs(os.path.join(server_path,currfolder))
+                print(os.path.join(server_path,currfolder))
+                shutil.copy2(os.path.join(path, file),os.path.join(server_path,currfolder))
+                print()
+        elif os.path.isdir(os.path.join(path, file)):
+            Check_Files(os.path.join(path,file), currfolder)
+
+with open("synctime.txt", "r") as file:
+    try:
+        lasttime = float(file.readline())
+    except:
+        lasttime = 0.0
+
 currtime = time.time()
 
-file = open("synctime.txt", "w") 
-file.write(str(currtime))
-file.close()
+with open("synctime.txt", "w") as file: 
+    file.write(str(currtime))
 
 paths = list()
-file = open("paths.txt","r")
-line = file.readline().replace("\n","")
-while line:
-    paths.append(line)
+with open("paths.txt","r") as file:
+    server_path = file.readline().replace("\n","")
     line = file.readline().replace("\n","")
-for path in paths:
-    onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    while line:
+        paths.append(line)
+        line = file.readline().replace("\n","")
 
-    filew = open("debug.txt","w")
-    for file in onlyfiles:
-        if os.path.getmtime(os.path.join(path, file)) > lasttime:
-            filew.write(os.path.join(path, file))
+for path in paths:
+    Check_Files(path,"")
